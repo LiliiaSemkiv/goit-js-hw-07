0411b866cc66e90ee6e4f6c8d7f75dc195e16977
LiliiaSemkiv <127901106+LiliiaSemkiv@users.noreply.github.com>
2023-08-18 17:56:31 +0200

    work

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,51 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery')
+// console.log(gallery)
+
+
+const markup = galleryItems.map(({preview, original, description}) => `<li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" data-source="${original}" src="${preview}" alt="${description}" ></a></li>`).join(''); // width="300"
+
+gallery.insertAdjacentHTML('beforeend', markup)
+
+
+gallery.addEventListener('click', onClick)
+
+
+function onClick(e) {
+    e.preventDefault();
+    if (e.target.classList.contains('gallery__image')) {
+        const source = e.target.dataset.source;
+        const instance = basicLightbox.create(`<img src="${source}" alt="Image">`);
+        instance.show();
+    }
+}
+
+const instance = basicLightbox.create(`
+    <h1>Dynamic Content</h1>
+    <p>You can set the content of the lightbox with JS.</p>
+`)
+
+// console.log(instance)
+
+instance.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,19 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery')
+// console.log(gallery)
+
+
+const markup = galleryItems.map(({preview, original, description}) => `<li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" data-source="${original}" src="${preview}" alt="${description}" ></a></li>`).join(''); // width="300"
+
+gallery.insertAdjacentHTML('beforeend', markup)
+
+
+let lightbox = new SimpleLightbox('.gallery a', {
+    captions: "true",
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,
+});
